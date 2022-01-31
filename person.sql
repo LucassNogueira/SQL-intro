ebf68d21fadfebcecf6677fd458d7fb685edf8aa
LucassNogueira <simplylucasjason@gmail.com>
2022-01-31 14:17:33 -0600

    Rip my spelling on favorite

--- person.sql
+++ person.sql
@@ -2,9 +2,10 @@ CREATE TABLE person (
     person_id SERIAL PRIMARY KEY,
     person_name VARCHAR(15),
     age INT,
-    height INT,
-    city VARCHAR(10)
+    height FLOAT,
+    city VARCHAR(10),
     favoirite_color VARCHAR(15)
 );
 
 
+INSERT INTO person ()
