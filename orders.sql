d3ce94e129e8d10fcc8bf1003b7d56cc3e1c1a1d
LucassNogueira <simplylucasjason@gmail.com>
2022-01-31 14:38:31 -0600

    2 done

--- orders.sql
+++ orders.sql
@@ -5,3 +5,12 @@ CREATE TABLE orders (
     product_price FLOAT,
     quantity INT
 );
+
+
+
+INSERT INTO orders (person_id,product_name, product_price,quantity)
+VALUES (1, 'Mic', 50.99, 2),
+(1, 'Headphones', 49.99, 2),
+(2, 'Mic', 50.99, 1),
+(3, 'Mouse', 30.99, 1),
+(4, 'Keyboard', 50.99, 1);
