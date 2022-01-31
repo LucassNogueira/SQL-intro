76b32cb25a426c33306ebae94a7a6c85b2c07a7b
LucassNogueira <simplylucasjason@gmail.com>
2022-01-31 16:09:19 -0600

    5 done

--- update.sql
+++ update.sql
@@ -14,3 +14,9 @@ WHERE first_name = 'Julia' AND last_name ='Barnett';
 UPDATE customer
 SET support_rep_id = 4
 WHERE email = 'luisrojas@yahoo.cl';
+
+
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = 3 AND composer = NULL
+
