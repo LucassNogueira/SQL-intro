8498b8231da4beba13a8b69730446eae00989351
LucassNogueira <simplylucasjason@gmail.com>
2022-01-31 16:17:01 -0600

    delete 4 done

--- delete.sql
+++ delete.sql
@@ -1,13 +1,4 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+
 
 
 
@@ -17,3 +8,9 @@ name VARCHAR(15),
  type VARCHAR(15),
 age INT
 );
+
+
+
+DELETE
+FROM animals
+WHERE type = 'lion';
