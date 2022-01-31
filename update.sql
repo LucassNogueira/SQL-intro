c0f2fce0d2265567d009172f77dca3778f07f1db
LucassNogueira <simplylucasjason@gmail.com>
2022-01-31 16:04:41 -0600

    4 done

--- update.sql
+++ update.sql
@@ -9,3 +9,8 @@ WHERE company = NULL;
 UPDATE customer
 SET last_name = 'Thompson'
 WHERE first_name = 'Julia' AND last_name ='Barnett';
+
+
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl';
